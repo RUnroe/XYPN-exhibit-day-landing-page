2f6ea0667277b43901656f11dd04ce7c3dd2df4f
RUnroe <ryanunroe@gmail.com>
2021-03-10 12:45:25 -0700

    refactor data into its own object

---
+++ data.js
@@ -0,0 +1,31 @@
+const xypnData = {
+    boothPackages: [
+        {
+            packageName: "Solutions",
+            packageDescription: "For insurance providers & investment solutions companies. Space is limited to 15 companies.",
+            pricing: [
+                {price:9000, startDate:"11/12/20", endDate:"12/31/20" },
+                {price:11500, startDate:"01/01/21", endDate:"05/31/21" },
+                {price:14000, startDate:"06/01/21", endDate:"11/10/21" }
+            ]
+        },
+        {
+            packageName: "Tech & General",
+            packageDescription: "For general exhibitors & tech companies. Space is limited.",
+            pricing: [
+                {price:4200, startDate:"11/12/20", endDate:"12/31/20" },
+                {price:5400, startDate:"01/01/21", endDate:"05/31/21" },
+                {price:6500, startDate:"06/01/21", endDate:"11/10/21" }
+            ]
+        },
+        {
+            packageName: "Newcomer",
+            packageDescription: "For start-ups launched after September 2020 or with annual revenue less than $500K. Space is limited.",
+            pricing: [
+                {price:1900, startDate:"11/12/20", endDate:"12/31/20" },
+                {price:2400, startDate:"01/01/21", endDate:"05/31/21" },
+                {price:2900, startDate:"06/01/21", endDate:"11/10/21" }
+            ]
+        },
+    ]
+}
--- script.js
+++ script.js
@@ -6,48 +6,94 @@ const getFormattedDate = date => {
 
 
 //Set text and style of price paragraphs based on the date
-document.querySelectorAll(".pricing").forEach(pricingContainer => {
+const createPriceTableRow = price => {
+    let row = document.createElement("tr");
+    //Set text of price
+    let priceDisplay = document.createElement("p");
+    priceDisplay.innerHTML = "$" + price.price;
+    //Put paragraph in table data
+    let tableData = document.createElement("td");
+    tableData.appendChild(priceDisplay);
+    row.appendChild(tableData);
 
-    //Loop over every child of pricing container
-    for(let i = 0; i < pricingContainer.children.length; i++) {
-        //Get current pricing row
-        let pricingRow = pricingContainer.children[i];
-        
-        //Set text of price
-        let priceDisplay = document.createElement("p");
-        priceDisplay.innerHTML = pricingRow.dataset.price;
-        //Append to table
-        let tableData = document.createElement("td");
-        tableData.appendChild(priceDisplay);
-        pricingRow.appendChild(tableData);
-        
+    //Set text of date
+    let dateDisplay = document.createElement("p");
+    let startDate = new Date(price.startDate);
+    let endDate = new Date(price.endDate);
+    let currentDate = new Date();
 
-
-        //Set text of date
-        let dateDisplay = document.createElement("p");
-        let startDate = new Date(pricingRow.dataset.startDate);
-        let endDate = new Date(pricingRow.dataset.endDate);
-        let currentDate = new Date();
-
-        //If the end date has already passed
-        if(endDate < currentDate) {
-            dateDisplay.innerHTML = "Until " + getFormattedDate(endDate);
-            pricingRow.classList.add("past");
-        }
-        //Check for the current pricing period (current date is between start and end date)
-        else if(startDate < currentDate && endDate > currentDate) {
-            dateDisplay.innerHTML = "Until " + getFormattedDate(endDate);
-            pricingRow.classList.add("current");
-        }
-        //Pricing period has not started yet
-        else {
-            dateDisplay.innerHTML = "Starts " + getFormattedDate(startDate);
-        }
-        //Append to table
-        tableData = document.createElement("td");
-        tableData.appendChild(dateDisplay);
-        pricingRow.appendChild(tableData);
+    //If the end date has already passed
+    if(endDate < currentDate) {
+        dateDisplay.innerHTML = "Until " + getFormattedDate(endDate);
+        row.classList.add("past");
     }
-});
+    //Check for the current pricing period (current date is between start and end date)
+    else if(startDate < currentDate && endDate > currentDate) {
+        dateDisplay.innerHTML = "Until " + getFormattedDate(endDate);
+        row.classList.add("current");
+    }
+    //Pricing period has not started yet
+    else {
+        dateDisplay.innerHTML = "Starts " + getFormattedDate(startDate);
+        row.classList.add("future");
+    }
+    //Append to table
+    tableData = document.createElement("td");
+    tableData.appendChild(dateDisplay);
+    row.appendChild(tableData);
+
+    return row;
+}
 
 
+
+
+// Dynamically create booth packages display based on data in xypnData object
+const createBoothPackages = () => {
+    xypnData.boothPackages.forEach( package => {
+        //Create package container
+        let container = document.createElement("div");
+        container.classList.add("package");
+        
+        //Create package title
+        let title = document.createElement("h2");
+        title.innerHTML = package.packageName;
+        container.appendChild(title);
+
+        //Create package description
+        let description = document.createElement("p");
+        description.innerHTML = package.packageDescription;
+        container.appendChild(description);
+
+        //Create package pricing title
+        let pricingTitle = document.createElement("h3");
+        pricingTitle.innerHTML = "Pricing:";
+        container.appendChild(pricingTitle);
+
+        //Create package price table
+        let priceTable = document.createElement("table");
+        priceTable.classList.add("price-table");
+        
+        //Create package price table body
+        let tableBody = document.createElement("tbody");
+        tableBody.classList.add("pricing");
+
+        //Create package price table elements
+        package.pricing.forEach(price => {
+            tableBody.appendChild(createPriceTableRow(price));
+        });
+
+        priceTable.appendChild(tableBody);
+        container.appendChild(priceTable);
+
+        //append package display to screen
+        document.getElementsByClassName("packages-list")[0].appendChild(container);
+    });
+}
+
+
+
+
+
+
+createBoothPackages();
